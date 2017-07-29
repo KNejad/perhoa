e67fc3f4a825a55692b1b92f0095235a87a08622
Keeyan Nejad <keeyan@knejad.co.uk>
2017-07-29 14:52:56 +0100

    Alarm refactor

--- helper.rb
+++ helper.rb
@@ -8,11 +8,9 @@ end
 def alarm config
   Thread.new do
     alarm_time = Time.parse(config[:time])
-    if alarm_time - Time.now > 0 
-      sleep_time = alarm_time - Time.now
-    else
-      sleep_time = alarm_time + (24*60*60) - Time.now
-    end
+    alarm_time += (24*60*60) if alarm_time - Time.now < 0 
+    
+    sleep_time = alarm_time - Time.now
     puts "Alarm set for " + alarm_time.to_s
     sleep(sleep_time)
     pid = fork{ exec 'mpg123','-q', asset('alarm.mp3') }
