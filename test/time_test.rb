797b7a0cdc17c3dc852a6ee0eea8500fa8a41ef9
Keeyan Nejad <keeyan@knejad.co.uk>
2017-07-30 18:56:13 +0100

    Passing test

--- test/time_test.rb
+++ test/time_test.rb
@@ -15,9 +15,9 @@ describe Alarm do
     delta = date >= Date.today ? 0 : 7
     date + delta
     date_string = date.strftime('%Y-%m-%d')
-    Thread.stub(:initialize, nil) do
+    Thread.stub(:new, nil) do
       Kernel.stub(:loop, nil) do
-        Kernel.stub(:sleep, sleep(1)) do
+        Kernel.stub(:sleep, nil) do
           Kernel.stub(:exec, nil) do 
             alarm_output = "Alarm 'name' set for Mon "+ date_string + " at 23:59:59\n"
             assert_output(alarm_output) { Alarm.new(config) }
