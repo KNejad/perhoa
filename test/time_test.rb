167f6106e4511fe4963e03050d82d0ea31a0e8f4
Keeyan Nejad <keeyan@knejad.co.uk>
2017-07-30 19:37:31 +0100

    Added test_disabled_alarm and test_multiple_alarms

--- test/time_test.rb
+++ test/time_test.rb
@@ -25,6 +25,61 @@ describe Alarm do
       assert_output(alarm_output) { Alarm.new(config) }
     end
   end
+
+  def test_multiple_alarms
+    now = DateTime.now  
+    alarm_1 = DateTime.new(now.year, 
+                              now.month,
+                              now.day,
+                              now.hour,
+                              now.minute + 1,
+                              now.second,
+                              now.zone)
+    alarm_2 = alarm_1 + 1
+    config = { 
+      "alarm 1": {
+        enabled: true,
+        time: alarm_1.strftime("%H:%M:%S"),
+        day: alarm_1.strftime('%a')
+      },
+      "alarm 2": {
+        enabled: true,
+        time: alarm_2.strftime("%H:%M:%S"),
+        day: alarm_2.strftime('%a')
+      }
+    }
+
+    alarm_output = 
+      "Alarm 'alarm 1' set for "  +  alarm_1.strftime('%a %Y-%m-%d at %H:%M:%S') + "\n" +
+      "Alarm 'alarm 2' set for "  +  alarm_2.strftime('%a %Y-%m-%d at %H:%M:%S') + "\n"
+    date_stubs do
+      assert_output(alarm_output) { Alarm.new(config) }
+    end
+  end
+
+
+  def test_disabled_alarm
+    now = DateTime.now  
+    alarm_1 = DateTime.new(now.year, 
+                              now.month,
+                              now.day,
+                              now.hour,
+                              now.minute + 1,
+                              now.second,
+                              now.zone)
+    config = { 
+      "alarm 1": {
+        enabled: false,
+        time: alarm_1.strftime("%H:%M:%S"),
+        day: alarm_1.strftime('%a')
+      },
+    }
+
+    alarm_output = ""
+    date_stubs do
+      assert_output(alarm_output) { Alarm.new(config) }
+    end
+  end
 end
 
 def date_stubs 
